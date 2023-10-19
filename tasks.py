99cd3a634bebb44717b2b05a4e66cc7a832ff0ad
Timothy Lopas <tim@robocorp.com>
2023-10-19 14:22:30 -0400

    Updating to make the rcc location discoverable based on rcc location, not system PATH

--- tasks.py
+++ tasks.py
@@ -4,6 +4,7 @@ import os
 
 @task
 def minimal_task():
+    rcc_location = os.environ.get("RCC_EXE", "rcc")
 
     # # Option 1 using the os.system commands
     # # While this is the simplest this also provides the least options
@@ -23,7 +24,7 @@ def minimal_task():
 
     # Option 3 using subprocess.Popen
     # This is the most complex option but provides the developer the most flexibility
-    metbot_run = subprocess.Popen(["rcc run --space metarobot --task metarobot"], shell=True, cwd=r"/Users/tim/Documents/Robot Tests/No Data Leaving Prem Bot Run/metarobot")
+    metbot_run = subprocess.Popen([f"{rcc_location} run --space metarobot --task metarobot"], shell=True, cwd=r"/Users/tim/Documents/Robot Tests/No Data Leaving Prem Bot Run/metarobot")
     output, errors = metbot_run.communicate()
     metbot_run.wait()
     print(output)
